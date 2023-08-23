554924c0582a1c24011151c31b06b2d880e6b38b
Shubham Kalson <92200512+ShubhamKalson@users.noreply.github.com>
2023-08-23 09:54:07 +0530

    Update and rename EmployeeTest.java to CaseStudy_EmployeeTest.java

--- CaseStudy_EmployeeTest.java
+++ CaseStudy_EmployeeTest.java
@@ -1,7 +1,7 @@
 package company.casestudy;
 import company.employee.Employee; 
 
-public class EmployeeTest {
+public class CaseStudy_EmployeeTest {
     public static void main(String[] args) {
         Employee employee = new Employee("Shubham", 30, "Software Developer", 50000.0);
         System.out.println("Employee Information:");
